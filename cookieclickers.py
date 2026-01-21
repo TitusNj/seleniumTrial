f80f2f4d65e369c7828ff127a0f832fb7778983a
Titusnj <titusnj99@gmail.com>
2026-01-22 01:35:17 +0300

    Initial commit - Selenium cookie bot

--- cookieclickers.py
+++ cookieclickers.py
@@ -1,49 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 import time
 
-service = Service(executable_path="chromedriver.exe")
+service = Service("/usr/local/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.get("https://orteil.dashnet.org/cookieclicker")
 
-# https://orteil.dashnet.org/cookieclicker/
+time.sleep(10)
+driver.quit()
+# 
 
-cookie_id = "bigCookie"
-cookies_id = "cookies"
-product_price_prefix = "productPrice"
-product_prefix = "product"
+# cookie_id = "bigCookie"
+# cookies_id = "cookies"
+# product_price_prefix = "productPrice"
+# product_prefix = "product"
 
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
-)
+# WebDriverWait(driver, 5).until(
+#     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
+# )
 
-language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
-language.click()
+# language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+# language.click()
 
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, cookie_id))
-)
+# WebDriverWait(driver, 5).until(
+#     EC.presence_of_element_located((By.ID, cookie_id))
+# )
 
-cookie = driver.find_element(By.ID, cookie_id)
+# cookie = driver.find_element(By.ID, cookie_id)
 
-while True:
-    cookie.click()
-    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
-    cookies_count = int(cookies_count.replace(",", ""))
+# while True:
+#     cookie.click()
+#     cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+#     cookies_count = int(cookies_count.replace(",", ""))
     
-    for i in range(4):
-        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+#     for i in range(4):
+#         product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
 
-        if not product_price.isdigit():
-            continue
+#         if not product_price.isdigit():
+#             continue
 
-        product_price = int(product_price)
+#         product_price = int(product_price)
 
-        if cookies_count >= product_price:
-            product = driver.find_element(By.ID, product_prefix + str(i))
-            product.click()
-            break
+#         if cookies_count >= product_price:
+#             product = driver.find_element(By.ID, product_prefix + str(i))
+#             product.click()
+#             break
---
+++ google.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+service = Service("/usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://google.com")
+
+time.sleep(10)
+driver.quit()
